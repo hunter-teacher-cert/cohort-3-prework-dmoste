e63a2461df38a6048fb63a581f77d4eebf028707
David Moste <60351405+dmoste@users.noreply.github.com>
2022-06-22 13:27:37 +0000

    added Loops.java

--- pre06/Loops.java
+++ pre06/Loops.java
@@ -22,26 +22,17 @@ public class Loops{
 		return result;
 	}
 	
-	public static double myexp(double x, int n){
-		double result = 1;
-		double priorNum = 1;
-		double priorDen = 1;
+	public static double squareRoot(double a){
+		double initial_guess = a/2;
+    double new_guess = 1;
 		
-		for(int i = 1; i <= n; i++){
-			double numerator = priorNum*x;
-			double denominator = priorDen*i;
+		for(int i = 1; i <= 10; i++){
+			new_guess = (initial_guess + (a/initial_guess))/2;
 			
-			result = result + (numerator/denominator);
-			
-			priorNum = numerator;
-			priorDen = denominator;
+			initial_guess = new_guess;
 		}
 		
-		return result;
-	}
-	
-	public static void check(double x){
-		System.out.println(x + "\t" + myexp(x, 17) + "\t" + Math.exp(x));
+		return new_guess;
 	}
 	
 	public static void main(String[] args){
@@ -51,19 +42,7 @@ public class Loops{
 		double y = factorial(7);
 		System.out.println(y);
 		
-		double z = myexp(4.0,10);
+		double z = squareRoot(9);
 		System.out.println(z);
-		
-		for(int i = -1; i < 3; i++){
-			double a = Math.pow(10, i);
-			
-			check(a);
-		}
-		
-		for(int i = -1; i < 3; i++){
-			double a = -Math.pow(10, i);
-			
-			check(a);
-		}
 	}
 }
